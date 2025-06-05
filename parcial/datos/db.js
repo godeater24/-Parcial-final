54d1b2e8f8619030af0a43081baef57ac122a504
brayan bejarano <122427585+godeater24@users.noreply.github.com>
2025-06-04 23:33:26 -0500

    Delete parcial directory

--- parcial/datos/db.js
+++
@@ -1,8 +0,0 @@
-export const datos = [
-  { id: 1, nombre: 'Camisa Blanca', categoria: 'Superior', precio: 250, estado: 'activo' },
-  { id: 2, nombre: 'Blusa Azul', categoria: 'Superior', precio: 300, estado: 'activo' },
-  { id: 3, nombre: 'Pantalón Negro', categoria: 'Inferior', precio: 400, estado: 'activo' },
-  { id: 4, nombre: 'Falda Roja', categoria: 'Inferior', precio: 350, estado: 'activo' },
-  { id: 5, nombre: 'Camisa Cuadros', categoria: 'Superior', precio: 275, estado: 'inactivo' },
-  { id: 6, nombre: 'Jeans', categoria: 'Inferior', precio: 450, estado: 'activo' }
-];
